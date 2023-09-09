a2402aaa388bc8663b4f9bcbb02307b953d3db40
vaibhav <rohitlondhe727@gmail.com>
2023-09-09 23:27:16 +0530

    ready invoice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component, Fragment } from 'react';
+import {PDFViewer} from '@react-pdf/renderer'
+import Invoice from './components/reports/Invoice'
+import invoice from './data/invoice'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import logo from './logo.svg';
+import './App.css';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends Component {
+  render() {
+    return (
+        <Fragment>
+            <PDFViewer width="1000" height="600" className="app" >
+                <Invoice invoice={invoice}/>
+            </PDFViewer>
+        </Fragment>
+    );
+  }
 }
 
-export default App
+export default App;
